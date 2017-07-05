54a909b1c97ebc525bdb0bbd45ecbc3cc881bb24
ChienPhamm97 <minhchienpham97@gmial.com>
2017-07-06 06:43:15 +0700

    Tao moi Project

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () { return view('welcome'); });
+Auth::routes();
+Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => ['auth']], function (){
+    //login thi vao duoc ko can phai la admin
+
+    Route::group(['middleware' => ['ckAdmin']], function (){
+        //login va phai la admin
+        Route::get('admin', 'Admin\DashboardController@index');
+        Route::get('admin/category', 'Admin\CategoryController@index');
+    });
+});
